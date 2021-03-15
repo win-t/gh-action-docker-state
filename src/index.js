f493bc060ca72309d388c9a340561f3f5fe63bdd
Kurnia D Win <kurnia.d.win@gmail.com>
2021-03-15 16:20:46 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
 
 const { spawn } = require('child_process')
 const { once } = require('events')
-const { saveState, getState, getInput, setOutput, error, info } = require('@actions/core')
+const { saveState, getState, error, info } = require('@actions/core')
 const { saveCache, restoreCache } = require('@actions/cache')
 
-const prefix = 'var-lib-docker-'
+const stateCacheKey = 'anz8sx6yyk'
+const stateExactMatch = 'yctvb1ynp1'
+const prefix = 'd8ubf9owv2-'
 
 const sudoShell = async script => {
   const shell = spawn('sudo', ['-n', 'sh', '-ceu', script], { stdio: 'inherit' })
@@ -30,23 +32,20 @@ const main = async () => {
 
   await sudoShell(`mv /var/lib/docker /var/lib/docker.bak && ( setsid rm -rf /var/lib/docker.bak </dev/null >/dev/null 2>&1 & )`)
 
-  const primaryKey = getInput('key') || prefix + process.env["GITHUB_SHA"]
-  saveState('primaryKey', primaryKey)
+  const cacheKey = prefix + (process.env["GITHUB_SHA"] || Math.random())
+  saveState(stateCacheKey, cacheKey)
 
-  const restoredKey = await restoreCache(['/var/tmp/docker-state.tar'], primaryKey, [prefix])
-  if (restoredKey) {
-    setOutput('cache-hit', true)
-    if (restoredKey == primaryKey) { saveState('exactMatch', true) }
+  const cacheHit = await restoreCache(['/var/tmp/docker-state.tar'], cacheKey, [prefix])
+  if (cacheHit) {
+    if (cacheHit == cacheKey) { saveState(stateExactMatch, true) }
     await sudoShell(`cd / && tar --numeric-owner -x -P -f /var/tmp/docker-state.tar && ( setsid rm -rf /var/tmp/docker-state.tar </dev/null >/dev/null 2>&1 & )`)
-  } else {
-    setOutput('cache-hit', false)
   }
 
   await startDocker()
 }
 
 const post = async () => {
-  if (getState('exactMatch')) {
+  if (getState(stateExactMatch)) {
     info('Not saving /var/lib/docker because previous cache is available')
     return
   }
@@ -56,26 +55,27 @@ const post = async () => {
   info('Saving /var/lib/docker')
 
   await sudoShell(`rm -rf /var/tmp/docker-state.tar && tar --numeric-owner -c -P -f /var/tmp/docker-state.tar /var/lib/docker`)
-  await saveCache(['/var/tmp/docker-state.tar'], getState('primaryKey'))
+  await saveCache(['/var/tmp/docker-state.tar'], getState(stateCacheKey))
 
   await startDocker()
 }
 
-const entrypoint = async () => {
-  switch (getState('nextEntrypoint')) {
-    case '':
-      saveState('nextEntrypoint', 'post')
-      return await main()
-    case 'post':
-      return await post()
-  }
-}
-
 if (require.main == module) {
-  entrypoint().catch(e => {
-    error(`global error handler: ${e}`)
-    process.exit(1)
-  })
+  (async () => {
+    try {
+      switch (getState('entrypointNext')) {
+        case '':
+          saveState('entrypointNext', 'post')
+          return await main()
+        case 'post':
+          return await post()
+      }
+    } catch (e) {
+      error(`global error handler: ${e}`)
+      process.exit(1)
+    }
+  })()
 } else {
-  exports.entrypoint = entrypoint
+  exports.main = main
+  exports.post = post
 }
